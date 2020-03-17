b262be8016766e80199ab6ea922fc81e99a5d94b
pun2pun <35132090+pun2pun@users.noreply.github.com>
2020-03-18 00:47:43 +0700

    Add files via upload

--- Get_image.py
+++ Get_image.py
@@ -4,10 +4,8 @@ import os
 import numpy as np
 from PIL import Image
 
-
 def Start_get_data(ids):
-        
-        cam = cv2.VideoCapture(0)
+        cam = cv2.VideoCapture(1)
         cam.set(3, 400)
         cam.set(4, 400) 
         face_detector = cv2.CascadeClassifier('Material/haarcascade_frontalface_default.xml')
@@ -15,21 +13,19 @@ def Start_get_data(ids):
         image_number = 20
         count = 0
         window_name = 'Get new face'
-        ofset_crop = 10
-        #print(face_id)
         while(True):
             ret, img = cam.read()
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             faces = face_detector.detectMultiScale(gray, 1.3, 2)
             for (x,y,w,h) in faces:
                 cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)   
-
+                '''
                 font = cv2.FONT_HERSHEY_SIMPLEX
                 percen_process = " {0:0.2f}%".format(round( (count/image_number) * 100 ))
                 cv2.putText(img, percen_process , (x+5,y-5), font, 1, (255,255,255), 2)  
                 count += 1
-                cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h+ofset_crop,x:x+w+ofset_crop])
-            
+                cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
+                '''
             cv2.imshow(window_name, img)
             k = cv2.waitKey(100) & 0xff
             if k == 27:
@@ -40,4 +36,3 @@ def Start_get_data(ids):
         cv2.destroyAllWindows()     
         print('Get data complete')
 
-Start_get_data('30')
--- Main.py
+++ Main.py
@@ -1,78 +1,65 @@
 import cv2
 
+
+def tell_Me_is_who(img,gray,faces,recognizer):
+    for(x,y,w,h) in faces:                                                  
+        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)                        # สร้างกรอบสี่เหลี่ยมที่ตำแหน่ง x,y และมีขนาดเท่ากับ w,h สีเขียว ความหนาเส้น 2 pixel
+        id, confidence = recognizer.predict(gray[y:y+h,x:x+w])                    # เรียกใช้ predict ละจะได้ค่า id และ confidence กลับมา
+        
+        if (confidence < 90 ):                                                    # ตั้งเงื่อนไขตามความเหมาะสม
+            confidence = "  {0}%".format(round(100 - confidence))                 # ปรับค่า confidence ให้เป็น %
+            if(id == 10):
+                name = 'Back Panter'
+            if(id == 20):
+                name = 'Buckky'
+            if(id == 30):
+                name = 'Captain'
+
+            font = cv2.FONT_HERSHEY_SIMPLEX                                          # เลือก font ให้ตัวหนังสือ 
+            cv2.putText(img, str(name), (x+5,y-5), font, 1, (0,255,0), 2)            # เพิ่มหนังสือ โดยให้เป็นชื่อ
+            cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)   # เพิ่มหนังสือ โดยให้เป็นความแม่นยำ                                                                                          
+        else:
+            name = "unknown"
+            confidence = "  {0}%".format(round( 100 - confidence ))
+    return img
+
+
+##-----------------------------------------------------------------------------------------------------------
+
+
 def begin_scan():
         
-        recognizer = cv2.face.LBPHFaceRecognizer_create()
-        faceCascade = cv2.CascadeClassifier("Material/haarcascade_frontalface_default.xml")
-        out = cv2.VideoWriter('output.mp4', -1, 20.0, (640,360))
-        try:
-            recognizer.read('trainModel/model.yml')
-        except:
-            print('Please check you file Model or Casecade')
-
-
-        font = cv2.FONT_HERSHEY_SIMPLEX
+        faceCascade = cv2.CascadeClassifier("Material/haarcascade_frontalface_default.xml")   #เรียนกใช้ไฟล์ Cascade ของใบหน้า
         
-        cam = cv2.VideoCapture(0)
-        cam.set(3, 640) # set video widht
-        cam.set(4, 480) # set video height
+        recognizer = cv2.face.LBPHFaceRecognizer_create()                           # เรียกใชฟังก์ชั่นการจดจำใบหน้า
+        recognizer.read('trainModel/Model.yml')                                     # โหลดค่ารูปแบบ Model ของใบหน้า
+       
+        cam = cv2.VideoCapture('avenger_vdo.mp4')                        # กำหนดแหล่งที่มาของ Vedio
+        cam.set(3, 640)                                                  # กำหนดขนาดความยาว = 640
+        cam.set(4, 480)                                                  # กำหนดขนาดความกว้าง = 480
 
         # Define min window size to be recognized as a face
-        minW = 0.1*cam.get(3)
+        minW = 0.1*cam.get(3)                                        
         minH = 0.1*cam.get(4)
-     
 
         while True:
-            ret, img =cam.read()
-            
-            gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+            ret, img =cam.read()                                          # อ่านค่า frame จาก Vedio
+            gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)                   # เปลียนรูปสี ให้เป็นแบบขาวดำ(Gray scale)
 
-            faces = faceCascade.detectMultiScale(gray,
+            faces = faceCascade.detectMultiScale(gray,                     
                 scaleFactor = 1.2,
                 minNeighbors = 5,
                 minSize = (int(minW), int(minH)),
                )
-            
-            for(x,y,w,h) in faces:
-                cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
-
-                id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
-                print(confidence)
-                if (confidence < 90 ):
-                    
-                    confidence = "  {0}%".format(round(100 - confidence))  
-                   
-                    if(id == 10):
-                        name = 'Back Panter'
-
-                    if(id == 20):
-                        name = 'Buckky'
-
-                    if(id == 30):
-                        name = 'Captain'
-
-                    cv2.putText(img, str(name), (x+5,y-5), font, 1, (0,255,0), 2)
-                    cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)                                                    
-                    
-                    #cv2.imwrite('image.jpg',img) 
-                        
-                else:
-                    name = "unknown"
-                    confidence = "  {0}%".format(round( 100 - confidence ))
-                
-                
-                
-                
+                       
+            img = tell_Me_is_who(img,gray,faces,recognizer)            # เรียกใช้ฟังก์ชั่น tell_Me_is_who ที่เราสร้างขึ้น
+                           
             window_name = 'camera'
             cv2.imshow(window_name,img) 
-            print(img.shape)
-            out.write(img)
-
+            
             k = cv2.waitKey(10) & 0xff 
-            if k == 27:        # Press 'ESC' for exiting video
+            if k == 27:                                                  # Press 'ESC' for exiting video
                 break
-
-       
         
         cam.release()
         cv2.destroyAllWindows()
@@ -82,3 +69,9 @@ def begin_scan():
 
 
 begin_scan()
+
+
+
+
+
+
